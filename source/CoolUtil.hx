caebef4a809b1e3888de1203f6e7f41131803efb
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-22 15:49:19 -0500

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -13,8 +13,7 @@ class CoolUtil
 {
 	// [Difficulty name, Chart file suffix]
 	public static var difficultyStuff:Array<Dynamic> = [
-		['Easy', '-easy'],
-		['Normal', ''],
+		['Alt', '-alt'],
 		['Hard', '-hard']
 	];
 
