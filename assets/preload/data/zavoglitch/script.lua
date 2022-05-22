49a448d296913674c5c44817974ad83b88a31e65
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-22 22:05:18 +0400

    Add files via upload

---
+++ assets/preload/data/zavoglitch/script.lua
@@ -0,0 +1,74 @@
+function opponentNoteHit(id,data,type,sus)
+    triggerEvent('Screen Shake','0.5, 0.03','0.01, 0.00')
+
+       health = getProperty('health')
+    if getProperty('health') > 0.1 then
+       setProperty('health', health- 0.02);
+	end
+end
+
+function onBeatHit()
+	if curBeat < 9999999999999999 then
+		triggerEvent('Add Camera Zoom', 0.04,0.05)
+
+		if curBeat % 2 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	else
+		setProperty('camHUD.angle',0)
+		setProperty('camHUD.x',0)
+		setProperty('camHUD.x',0)
+	end
+		
+end
+
+function onStepHit()
+	if curBeat < 388 then
+		if curStep % 4 == 0 then
+			doTweenY('rrr', 'camHUD', -12, stepCrochet*0.002, 'circOut')
+			doTweenY('rtr', 'camGame.scroll', 12, stepCrochet*0.002, 'sineIn')
+		end
+		if curStep % 4 == 2 then
+			doTweenY('rir', 'camHUD', 0, stepCrochet*0.002, 'sineIn')
+			doTweenY('ryr', 'camGame.scroll', 0, stepCrochet*0.002, 'sineIn')
+		end
+	end
+end
+
+function onCreate()
+    math.randomseed(os.clock() * 1000);
+    
+    --doTweenAlpha("gone","camHUD",0,0.01)
+end
+function onUpdate(elapsed)
+        if mustHitSection == false then
+            setProperty('defaultCamZoom',1.3)
+        else
+           
+            setProperty('defaultCamZoom',0.8)
+        end
+end
+
+--[[function onTweenCompleted(tag)
+	if string.find(tag,"wheeeup") then
+        for i = 0,7 do
+            local uhhh = curBeat % 8 * (i + i)
+            local swag = i % 4 * 2.5 - uhhh
+            if i > 3 then
+                x =  getPropertyFromGroup('opponentStrums', i-10, 'x');
+            else
+                x =  getPropertyFromGroup('playerStrums', i, 'x');
+            end
+            --noteTweenY("wheeeup"..i,i,y + turn,crochet*0.002,"sineInOut")
+            noteTweenX("wheeeleft"..i,i,x,crochet*0.002,"sineInOut")
+        end
+    end
+end]] 
